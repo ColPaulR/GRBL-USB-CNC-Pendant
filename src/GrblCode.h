44c35bcd6e5587d3c038f296408d977842313da8
ColPaulR <35244547+ColPaulR@users.noreply.github.com>
2024-01-18 16:02:01 -0500

    Update GrblCode.h

--- src/GrblCode.h
+++ src/GrblCode.h
@@ -4,11 +4,12 @@
 #include "GrblParserC.h"
 
 #define MAX_STATUS_LEN 10
+
 struct GrbltStatus
 {
   char  cStatus[MAX_STATUS_LEN];
-  double axis_WorkPosition[6];
-  double axis_MachinePosition[6];
+  pos_t axis_WorkPosition[MAX_N_AXIS];
+  pos_t axis_MachinePosition[MAX_N_AXIS];
   uint16_t spindle_speed;
 };
 
